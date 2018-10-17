4ed3b898f8f47a7ad9516c99738a5b0b216c4ee2
Palmer-Robert <robert.palmer1229@gmail.com>
2018-10-17 09:52:19 -0700

    Revision of the HttpURLConnection class.

--- src/cit360/HttpURLConnection.java
+++ src/cit360/HttpURLConnection.java
@@ -7,21 +7,31 @@ package cit360;
 
 import java.io.*;
 import java.net.*;
-import javax.net.ssl.HttpsURLConnection;
 
 /**
  *
  * @author Robert Palmer
  */
+
 // This class is focused on HttpURLConnections
 public class HttpURLConnection {
     public static void main(String[] args) throws Exception {
+    // Step 1: Create a URL object
     URL url = new URL("http://www.google.com");
+    
+    // Step 2: Obtain a URLConnection object from the URL
     URLConnection connect = url.openConnection();
+    
+    // Step 3: Configure the URLConnection
+    // Step 4: Read the header fields (Optional)
+    // Step 5: Get an input stream and read data (Optional)
+    // Step 6: Get an output stream and write data (Optional)
     BufferedReader in = new BufferedReader(new InputStreamReader(connect.getInputStream()));
     String inputLine;
     while ((inputLine = in.readLine()) != null)
         System.out.println(inputLine);
+    
+    // Step 7: Close the connection
     in.close();
     
     }
